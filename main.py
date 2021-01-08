ae3c8287625b4d371acc6f643db7eca2c5f3fdf3
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-01-07 21:13:16 -0500

    Added variables to adjust

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 # Randomly Plays Boss Music
 # This is terrible code, please do not try and replicate it
 
+# Change these variables
+
+# These are in seconds
+MinLength = 10800
+MaxLength = 25200
+
+
+# Bad Code
 import time, random
 from playsound import playsound
 import pyttsx3
-
 tts = pyttsx3.init()
 
 while True:
@@ -14,7 +21,7 @@ while True:
     tts.runAndWait()
 
     # Wait 3-7 hours
-    time.sleep(random.randint(3, 7) * 3600)
+    time.sleep(random.randint(MinLength, MaxLength))
     tts.say('Why do i hear boss music?')
     tts.runAndWait()
     playsound('./Music/Boss1.wav')
